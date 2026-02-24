db1e756e953fce0a94f3f2be228b81401a53378d
Tiru vardhan <150202554+tiru02@users.noreply.github.com>
2026-02-24 13:30:50 +0530

    updated loader

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -17,32 +17,29 @@ onUnmounted(() => {
 
 <template>
   <div v-if="loading" class="fixed inset-0 flex items-center justify-center bg-black z-50">
-    <div class="relative text-6xl font-extrabold text-white/10 overflow-hidden select-none px-10">
+    <div class="relative text-6xl font-extrabold text-white/10 overflow-visible select-none">
       जय श्री राम
       <span
         class="
           absolute 
           left-0 
           top-0 
-          text-amber-500 
+          text-[#FF8F29]
           overflow-hidden 
           whitespace-nowrap 
-          w-0 
           animate-fill-custom
-          border-r-4
-          px-10
-          border-amber-500
         "
       >
         जय श्री राम
       </span>
+      <div class=" bottom-0 translate-y-2 left-0 w-full h-1 bg-[#FF8F29] animate-fill-custom"></div>
     </div>
   </div>
 </template>
 
 <style scoped>
 @keyframes fillAnimation {
-  from { width: 0%; }
+  from { width: 0; }
   to { width: 100%; }
 }
 
